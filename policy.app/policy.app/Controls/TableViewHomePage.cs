5db915523e79a298e86529587a2f96a10fd8ce7d
Радик Ямалеев <iamaleev.r@yandex.ru>
2019-08-02 16:18:24 +0300

    Верстка профиля

---
+++ policy.app/policy.app/Controls/TableViewHomePage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace policy.app.Controls
+{
+    public class TableViewHomePage : TableView
+    {
+        public static BindableProperty SeparatorColorProperty = BindableProperty.Create("SeparatorColor", typeof(Color), typeof(TableViewHomePage));
+        public Color SeparatorColor
+        {
+            get
+            {
+                return (Color)GetValue(SeparatorColorProperty);
+            }
+            set
+            {
+                SetValue(SeparatorColorProperty, value);
+            }
+        }
+    }
+}
