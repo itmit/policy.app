c0d6b5c95eab4cb81f56f2f52191eff1f545ac11
Радик Ямалеев <iamaleev.r@yandex.ru>
2019-08-16 16:02:47 +0300

    Изменения внешнего вида страницы Рейтинга

---
+++ policy.app/policy.app/Controls/PickerRating.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace policy.app.Controls
+{
+    public class PickerRating : Picker
+    {
+        public static readonly BindableProperty FontSizeProperty =
+        BindableProperty.Create(nameof(FontSize), typeof(Int32), typeof(PickerRating), 24, BindingMode.TwoWay);
+
+        public Int32 FontSize
+        {
+            set { SetValue(FontSizeProperty, value); }
+            get { return (Int32)GetValue(FontSizeProperty); }
+        }
+
+        public static BindableProperty PlaceholderColorProperty =
+            BindableProperty.Create(nameof(PlaceholderColor), typeof(string), typeof(PickerRating), "#19365f", BindingMode.TwoWay);
+
+        public string PlaceholderColor
+        {
+            get { return (string)GetValue(PlaceholderColorProperty); }
+            set { SetValue(PlaceholderColorProperty, value); }
+        }
+    }
+}
