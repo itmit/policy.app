37768415a65e2a6a2b81154c15acc63db8345ed6
Радик Ямалеев <iamaleev.r@yandex.ru>
2019-08-21 11:12:35 +0300

    Изменения профиля

--- policy.app/policy.app/Pages/ProfilePage.xaml.cs
+++ policy.app/policy.app/Pages/ProfilePage.xaml.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using policy.app.Views;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -17,25 +13,9 @@ namespace policy.app.Pages
 			InitializeComponent();
 		}
 
-
-		private void Button_OnClicked(object sender, EventArgs e)
-		{
-			Navigation.PushModalAsync(new HomePage());
-		}
-
-		private void Button_OnClicked2(object sender, EventArgs e)
-		{
-			Navigation.PushModalAsync(new CatalogPage());
-		}
-
-		private void Button_OnClicked3(object sender, EventArgs e)
-		{
-			Navigation.PushModalAsync(new RatingPage());
-		}
-
-		private void Button_OnClicked4(object sender, EventArgs e)
-		{
-			Navigation.PushModalAsync(new AboutPage());
-		}
-	}
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Browser.OpenAsync("https://en.wikipedia.org/wiki/", BrowserLaunchMode.SystemPreferred);
+        }
+    }
 }
