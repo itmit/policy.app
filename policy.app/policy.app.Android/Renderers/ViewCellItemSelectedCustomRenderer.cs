8d061f243c07226b4c982c01dc794dc571a858d7
Радик Ямалеев <iamaleev.r@yandex.ru>
2019-08-23 18:01:05 +0300

    Меню

---
+++ policy.app/policy.app.Android/Renderers/ViewCellItemSelectedCustomRenderer.cs
@@ -0,0 +1,22 @@
+﻿using policy.app.Droid.Renderers;
+using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(ViewCell), typeof(ViewCellItemSelectedCustomRenderer))]
+
+namespace policy.app.Droid.Renderers
+{
+    public class ViewCellItemSelectedCustomRenderer : ViewCellRenderer
+    {
+        protected override Android.Views.View GetCellCore(Cell item, Android.Views.View convertView, Android.Views.ViewGroup parent, Android.Content.Context context)
+        {
+            var cell = base.GetCellCore(item, convertView, parent, context);
+
+            cell.SetBackgroundResource(Resource.Drawable.ViewCellBackground);
+
+            return cell;
+        }
+    }
+}
