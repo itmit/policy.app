1c4810e29b870e2283fe3240ec909f14c506533b
Радик Ямалеев <iamaleev.r@yandex.ru>
2019-09-10 17:57:44 +0300

    Правки внешнего вида

---
+++ policy.app/policy.app.Android/Renderers/ListViewNativeRenderer.cs
@@ -0,0 +1,52 @@
+﻿using Android.Content;
+using Android.Graphics.Drawables;
+using Android.Views;
+using policy.app.Controls;
+using policy.app.Droid.Renderers;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(NativeListView), typeof(ListViewNativeRenderer))]
+namespace policy.app.Droid.Renderers
+{
+    public class ListViewNativeRenderer : ViewCellRenderer
+    {
+        private Android.Views.View _cellCore;
+        private Drawable _unselectedBackground;
+        private bool _selected;
+
+        protected override Android.Views.View GetCellCore(Cell item,
+                                                          Android.Views.View convertView,
+                                                          ViewGroup parent,
+                                                          Context context)
+        {
+            _cellCore = base.GetCellCore(item, convertView, parent, context);
+
+            _selected = false;
+            _unselectedBackground = _cellCore.Background;
+
+            return _cellCore;
+        }
+
+        protected override void OnCellPropertyChanged(object sender, PropertyChangedEventArgs args)
+        {
+            base.OnCellPropertyChanged(sender, args);
+
+            if (args.PropertyName == "IsSelected")
+            {
+                _selected = !_selected;
+
+                if (_selected)
+                {
+                    var extendedViewCell = sender as NativeListView;
+                    _cellCore.SetBackgroundColor(extendedViewCell.SelectedItemBackgroundColor.ToAndroid());
+                }
+                else
+                {
+                    _cellCore.SetBackground(_unselectedBackground);
+                }
+            }
+        }
+    }
+}
