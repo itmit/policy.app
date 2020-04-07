4e2dc451f13e3f7eb745e50dbaba81408cd917e1
Sergey <32011415+leguha@users.noreply.github.com>
2020-04-07 15:24:35 +0300

    category

--- src/policy.app.Android/MainActivity.cs
+++ src/policy.app.Android/MainActivity.cs
@@ -1,8 +1,11 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.Gms.Common;
 using Android.OS;
 using Android.Runtime;
+using Android.Util;
 using FFImageLoading.Forms.Platform;
+using Firebase.Messaging;
 using ImageCircle.Forms.Plugin.Droid;
 using policy.app.Services;
 using Plugin.CurrentActivity;
@@ -29,6 +32,33 @@ namespace policy.app.Droid
 			base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 		}
 
+		internal static readonly string ChannelId = "my_notification_channel";
+		internal static readonly int NotificationId = 100;
+
+		private const string Tag = "GardenerMainActivity";
+
+		private void CreateNotificationChannel()
+		{
+			if (Build.VERSION.SdkInt < BuildVersionCodes.O)
+			{
+				// Notification channels are new in API 26 (and not a part of the
+				// support library). There is no need to create a notification
+				// channel on older versions of Android.
+				return;
+			}
+
+			var channel = new NotificationChannel(ChannelId,
+												  "FCM Notifications",
+												  NotificationImportance.Default)
+			{
+
+				Description = "Firebase Cloud Messages appear in this channel"
+			};
+
+			var notificationManager = (NotificationManager)GetSystemService(Android.Content.Context.NotificationService);
+			notificationManager.CreateNotificationChannel(channel);
+		}
+		 
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
 			CrossCurrentActivity.Current.Init(this, savedInstanceState);
@@ -36,6 +66,12 @@ namespace policy.app.Droid
 			ToolbarResource = Resource.Layout.Toolbar;
 
 			base.OnCreate(savedInstanceState);
+
+			CreateNotificationChannel();
+			var resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+			Log.Info(Tag, $"IsGooglePlayServicesAvailable code is {resultCode}");
+			FirebaseMessaging.Instance.SubscribeToTopic("all");
+
 			ImageCircleRenderer.Init();
 			Platform.Init(this, savedInstanceState);
 			Forms.Init(this, savedInstanceState);
